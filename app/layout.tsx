42c322ba086f1e9e6677f1b4d94eb9945514782e
Navin Chikkodi <73332645+og-nav@users.noreply.github.com>
2024-06-10 10:52:52 -0500

    undid export defaultUrl

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,10 @@ import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import Header from "@/components/header";
 
-const defaultUrlOrig = process.env.VERCEL_URL
+const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
-export const defaultUrl = 'https://quiz-gpt-psi.vercel.app';
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "QuizGPT",
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,5 +1,3 @@
-import { defaultUrl } from '@/app/layout';
-
 export const getURL = (path: string = '') => {
   // Check if NEXT_PUBLIC_SITE_URL is set and non-empty. Set this to your site URL in production env.
   let url =
